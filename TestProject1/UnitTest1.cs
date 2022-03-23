38096fc2dc840cf16effceb61e4d19619f40a022
xuanzhou2022 <102202011+xuanzhou2022@users.noreply.github.com>
2022-03-23 15:00:39 +1000

    ddd

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using test1;
+
+namespace TestProject1
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var m = new Manager();
+			var m2 = m.Add(1, 2);
+			Assert.IsTrue(m2 == 3);
+		}
+	}
+}
